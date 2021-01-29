9ba0a2eee12b97a956397758c0f2a918eabb8102
Xerbeth <faiber.torres.o@gmail.com>
2021-01-28 19:34:40 -0500

    Commit Inicial del proyecto

---
+++ ExceptionHandling/Operaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    public class Operaciones
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                OperacionesBasicas oB = new OperacionesBasicas();
+                Console.WriteLine(oB.Suma("12", "1,4"));
+                Console.WriteLine(oB.Resta("12y", "1,4"));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+        }
+
+    }
+}
---
+++ ExceptionHandling/OperacionesBasicas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class OperacionesBasicas
+    {
+        public float a { get; set; }
+        public float b { get; set; }
+
+        public OperacionesBasicas()
+        {
+            a = b = 0;
+        }
+
+        public float Suma(string a, string b)
+        {
+            try
+            {
+                this.a = TryParseNumber(a);
+                this.b = TryParseNumber(b);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message.ToString());
+            }            
+
+            return (this.a + this.b);
+        }
+
+        private float TryParseNumber(string number)
+        {
+            bool flag = float.TryParse(number, out float numberParse);
+            if (!flag)
+            {
+                GenerateException(number);
+            }
+            return numberParse;
+        }
+
+        public float Resta(string a, string b)
+        {
+            bool flag = float.TryParse(a, out float parseA);
+            if (!flag)
+            {
+                GenerateException(a);
+            }
+
+            this.a = parseA;
+
+            flag = float.TryParse(b, out float parseB);
+            if (!flag)
+            {
+                GenerateException(b);
+            }
+
+            this.b = parseB;
+
+            return (this.a + this.b);
+        }
+
+        /// <summary>
+        /// Método para generar las excepciones genericas 
+        /// </summary>
+        /// <param name="number"> Parametro no valida para convertir a float </param>
+        private void GenerateException(string number)
+        {
+            throw new ArgumentException("No es posible convertir el parametro con valor -> " + number);
+        }
+    }
+}
